2b8736a8f0e989257330c6baa1a4eecb50937adb
bsrayapici <bsrayapici95@gmail.com>
2025-06-14 09:23:58 +0300

    header changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+import HomePage from "./pages/Homepage";
 
+
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <Router>
+      <Header />
+      <PageContent>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* Add more routes here as needed */}
+      </Routes>
+      </PageContent>
+      <Footer />
+      </Router>
   )
 }
 
 export default App
+
---
+++ src/layout/Header.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import {Menu, X, ShoppingCart, User, Search} from "lucide-heart";
+
+const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    const navigation = ["Home", "Product", "Pricing", "Contact"];
+
+    return (
+        <header className="flex justify-between items-center p-4 shadow-md">
+            <div className="font-bold text-xl">Bandage</div>
+
+        </header>
+
+    );
+};
