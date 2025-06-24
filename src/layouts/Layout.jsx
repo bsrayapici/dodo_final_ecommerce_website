2ab300dde38deda3074e6234f247fc0e14708c0b
bsrayapici <bsrayapici95@gmail.com>
2025-06-24 17:39:33 +0300

    Breadcrumb, footer added to layout.jsx

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,21 @@
 import TopContact from "./TopContact";
+import { useLocation } from "react-router-dom";
 import Header from "./Header";
+import Footer from "./Footer";
+import Breadcrumb from "./Breadcrumb";
+
 
 export default function Layout({ children }) {
+
+  const location = useLocation();
+
   return (
     <div className="min-h-screen flex flex-col">
       <TopContact />
       <Header />
+      {location.pathname !== "/" && <Breadcrumb />}
       <main className="flex-grow bg-white">{children}</main>
+      <Footer />
     </div>
   );
 }
